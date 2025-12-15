45d8e61d400689ff9a5638db6741eecf091e1a3b
sourav-maji <s722143@gmail.com>
2025-12-15 11:09:57 +0530

    Fixing bug

--- server.py
+++ server.py
@@ -15,7 +15,7 @@ def chat(query : str = Query(...,description= "The chat query of user") ):
     job = queue.enqueue(process_query,query)
     return {"status" : "queued", "job_id": job.id}
 
-@app.get("job-status")
+@app.get("/job-status")
 def get_result(
         job_id: str = Query(...,description= "Job Id")
 ):
